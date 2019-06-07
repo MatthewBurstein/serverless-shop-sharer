6ce73d2ef6d0ef3446cd5086beff40d59e12af4d
Matt Burstein <j.matthew.burstein@gmail.com>
2019-06-07 23:13:45 +0100

    add amplify back-end

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
 import logo from './logo.svg';
 import './App.css';
+
+Amplify.configure(awsconfig)
+
+const signUpConfig = {
+  hiddenDefaults: ['phone_number']
+}
 
 function App() {
   return (
@@ -23,4 +32,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App, { signUpConfig });
